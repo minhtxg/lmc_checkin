aa411523f353ed5cefdf1007a13e5e2dbd6976e2
Bui Quang Minh Tam <buiminhtamvbk@gmail.com>
2024-08-08 20:37:19 +0700

    Init

---
+++ autoimportexport.lrdevplugin/Info.lua
@@ -0,0 +1,19 @@
+return {
+
+	LrSdkVersion = 3.0,
+	LrSdkMinimumVersion = 1.3, -- minimum SDK version required by this plug-in
+
+	LrToolkitIdentifier = 'nl.minhtxgenz.lightroom.autoimportexport',
+
+	LrPluginName = LOC "$$$/AutoImportExport/PluginName=Auto Exoport",
+
+	LrExportMenuItems = {
+		title = "Auto Import & Export",
+		file = "ExportMenuItem.lua",		
+	},
+	VERSION = { major=1, minor=0, revision=0, build=20220724, },
+
+}
+
+
+	
